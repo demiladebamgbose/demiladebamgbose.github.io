f1bbb7c8b4f7f6a3b18f55ff218c2877259e619d
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-05-30 07:32:45 +0100

    fix indentation

--- js/login.js
+++ js/login.js
@@ -3,12 +3,11 @@ function start(){
 	registerClickEvent('signIn', signin);
 	getId('msg').style.color='red';
 }
-	
+
 function signin(){
 	var found;
 	if( (isValidData('email')) && (isValidData('password')) ){
 		if(isEmail('email')){
-			console.log(getId('email').value.split('@')[0] );
 			var myUserRef = new Firebase('https://icalendra.firebaseio.com/'+ getId('email').value.split('@')[0]);
 			myUserRef.on("value", function(snapshot) {
 				var message = snapshot.val();	
@@ -16,8 +15,6 @@ function signin(){
 				if( (message.email == getId('email').value) && (message.password === getId('password').value)){
 					localStorage.setItem("database",getId('email').value.split('@')[0]);
 					localStorage.setItem('currentUser', message.fullName);
-					console.log(localStorage.getItem('database'));
-					console.log(localStorage.getItem('currentUser'));
 					linkTo('event.html');
 				}
 				else{
